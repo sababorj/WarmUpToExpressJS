1adb59b5f181c5ee97b489e36ecb03f17e1374ba
Saba Schock <sababorjsaba@gmail.com>
2018-12-13 14:07:05 -0800

    cleaning the code -  eliminating the repeatation

--- expressServer.js
+++ expressServer.js
@@ -3,19 +3,43 @@ var express = require('express');
 var app = express();
 var port = 2000;
 
-var person = {
+var person = [{
+    rout: 'mo',
+    name: 'mona',
+    age: '35',
+    title: 'Bussinees Woman'
+}, {
+    rout: 'sa',
     name: 'saba',
-    age: 'TBD',
+    age: '30',
     title: 'Engineer'
-}
+},
+{
+    rout: 'si',
+    name: 'sina',
+    age: '40',
+    title: 'Bussinees man'
+}];
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("Heloo there! Your exress looks good!")
 });
 
-app.get('/person', (req,res) => {
+app.get('/person', (req, res) => {
     res.json(person)
 });
+
+app.get('/person/:chosenPerson', (req, res) => {
+    var chosenPerson = req.params.chosenPerson;
+    console.log(chosenPerson);
+
+    for (var i = 0; i < person.length; i++) {
+        if (chosenPerson === person[i].rout) {
+            return res.json(person[i]);
+        }
+    }
+    return res.status(404).send('No Charactore found!');
+})
 
 app.listen(port, () => {
     console.log(`I am listening on port, ${port}`)
